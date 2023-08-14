f0e1fc885c27e479aa017fbf0920f177a9fa8272
Faisal A F Rahman <64533786+hann315@users.noreply.github.com>
2023-08-14 16:48:08 +0700

    remove some line

--- build_push_image.sh
+++ build_push_image.sh
@@ -2,23 +2,28 @@
 
 # Build docker image dengan nama item-app dan tag v1
 docker build -t item-app:v1 .
-echo '\n\n' #print line baru
+#print line baru
+echo '\n\n'
 
 # listing
 docker images
-echo '\n\n' #print line baru
+#print line baru
+echo '\n\n'
 
 # Membuat tag image dengan nama item-app:v1 dan tag ghcr.io/hann315/item-app:v1
 docker tag item-app:v1 ghcr.io/hann315/item-app:v1
-echo '\n\n' #print line baru
+#print line baru
+echo '\n\n'
 
 # Melakukan login ke github packages dengan menggunakan token
-export GITHUB_PACKAGES_TOKEN=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
+export GITHUB_PACKAGES_TOKEN=
 
 # Login ke github packages
 echo $GITHUB_PACKAGES_TOKEN | docker login ghcr.io -u hann315 --password-stdin
-echo '\n\n' #print line baru
+#print line baru
+echo '\n\n'
 
 # Melakukan push image ke github packages
 docker push ghcr.io/hann315/item-app:v1
-echo '\n\n' #print line baru
+#print line baru
+echo '\n\n'
