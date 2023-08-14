289c9fa0d3d67652accdd88d06f9c95dd624640d
Faisal A F Rahman <64533786+hann315@users.noreply.github.com>
2023-08-14 16:41:10 +0700

    submission

---
+++ build_push_image.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Build docker image dengan nama item-app dan tag v1
+docker build -t item-app:v1 .
+echo '\n\n' #print line baru
+
+# listing
+docker images
+echo '\n\n' #print line baru
+
+# Membuat tag image dengan nama item-app:v1 dan tag ghcr.io/hann315/item-app:v1
+docker tag item-app:v1 ghcr.io/hann315/item-app:v1
+echo '\n\n' #print line baru
+
+# Melakukan login ke github packages dengan menggunakan token
+export GITHUB_PACKAGES_TOKEN=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
+
+# Login ke github packages
+echo $GITHUB_PACKAGES_TOKEN | docker login ghcr.io -u hann315 --password-stdin
+echo '\n\n' #print line baru
+
+# Melakukan push image ke github packages
+docker push ghcr.io/hann315/item-app:v1
+echo '\n\n' #print line baru
