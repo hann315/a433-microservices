bcd378ea21dcb60fcd82a7405391194d300e1bec
Faisal A F Rahman <64533786+hann315@users.noreply.github.com>
2023-08-20 11:56:10 +0700

    add comments to shell script

--- build_and_push_order_service.sh
+++ build_and_push_order_service.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# variable yang akan menyimpan token github packages yang akan dibaca dari file github_token.txt oleh command cat
 GITHUB_PACKAGES_TOKEN=$(cat github_token.txt)
 
+# variable yang akan menyimpan nama repository
 REPO_NAME="hann315/order-service"
+# variable yang akan menyimpan versi package
 PACKAGE_VERSION="latest"
 
+# command yang akan menjalankan docker build dan memberikan tag pada image yang dibuat
 docker build -t ghcr.io/$REPO_NAME:$PACKAGE_VERSION .
 
+# command yang akan melakukan login ke github packages menggunakan token yang sudah disimpan di variable GITHUB_PACKAGES_TOKEN
 echo $GITHUB_PACKAGES_TOKEN | docker login ghcr.io -u hann315 --password-stdin
 
+# command yang akan melakukan push image ke github packages
 docker push ghcr.io/$REPO_NAME:$PACKAGE_VERSION
 
+# command yang akan melakukan logout dari github packages
 docker logout ghcr.io
